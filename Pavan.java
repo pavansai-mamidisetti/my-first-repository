3aed22188634b7c88ad4d5d10d28eaa969203692
pavankumar-sai <pavansaimamidisetti747@gmail.com>
2020-02-25 16:52:25 +0000

    first commit

---
+++ Pavan.java
@@ -0,0 +1,13 @@
+class Pavan
+{
+	public static void main(String args[])
+	{
+	  int i;
+	  System.out.println("printing 10th table");
+	  for(i=1;i<=10;i++)
+	  {
+        System.out.print("10*"+i+"=");
+        System.out.println(10*i);
+      }
+	}
+}
